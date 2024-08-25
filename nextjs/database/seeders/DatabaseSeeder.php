dce236b84be486145cf65d7a13ae0d44bc0e5841
daiyakawano <gomesuarehandoro0@gmail.com>
2024-08-25 23:10:38 +0900

    update 08/25

--- nextjs/database/seeders/DatabaseSeeder.php
+++ nextjs/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            PenSeeder::class,
+            // PenSeeder::class,
+            CustomerSeeder::class,
         ]);
     }
 }
